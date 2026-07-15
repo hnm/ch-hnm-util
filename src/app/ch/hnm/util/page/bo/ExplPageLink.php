09d65c1d87b7ad97c266c7fe6f6d9f54cdeb271c
Thomas Guenther <guenther@hnm.ch>
2026-07-15 12:26:01 +0200

    update für php 8.4

--- src/app/ch/hnm/util/page/bo/ExplPageLink.php
+++ src/app/ch/hnm/util/page/bo/ExplPageLink.php
@@ -98,7 +98,7 @@ class ExplPageLink extends ObjectAdapter implements UrlComposer {
 	 * @see \n2n\web\http\nav\UrlComposer::toUrl()
 	 */
 	public function toUrl(N2nContext $n2nContext, ?ControllerContext $controllerContext = null, 
-			string &$suggestedLabel = null): Url {
+			?string &$suggestedLabel = null): Url {
 		
 		if ($this->type == self::TYPE_EXTERNAL) {
 			$suggestedLabel = $this->label;
